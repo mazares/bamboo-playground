bd9340ae2d0e275c4d7333d8422daa0924767730
Stefan Mazare <84594267+mazares@users.noreply.github.com>
2024-09-17 13:59:26 +0300

    added components

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+import Nav from "../nav/Nav";
+
+export default function Header() {
+  return (
+    <header className="Header">
+      <p>header</p>
+      <Nav />
+    </header>
+  );
+}
